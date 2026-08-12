324284aea360d9a470689123b61460873ef50195
Ronal David Cucariano <ronalcucariano@gmail.com>
2026-08-12 01:49:32 +0000

    Inicio proyecto patronador

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgest import QApplication, QLabel, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PATRONADOR")
+        SELF.SETgEOMETRY(100, 100, 800, 500)
+
+        texto = QLabel("Aplicacion de patrones funcionando")
+
+        select.setCentralWidget(texto)
+
+app = QApplication(sys.argv)
+
+ventana = MainWindow()
+ventana.show()
+
+sys.exit(app.exec())
