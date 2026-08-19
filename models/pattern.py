2e37ce28efc82346aa4b0576a775de0d7bcfe8e3
Ronal David Cucariano <ronalcucariano@gmail.com>
2026-08-19 01:47:29 +0000

    avance en pattern con la validacion de colores y casillas

--- models/pattern.py
+++ models/pattern.py
@@ -3,13 +3,14 @@ class Pattern:
     Representa un patrón generado a partir de una imagen.
     """
 
-    def __init__(self, ancho, alto, matriz_colores):
+    def __init__(self, ancho, alto, matriz_colores, paleta_rgb=None):
         """
         Inicializa un patrón.
         """
         self.ancho = ancho
         self.alto = alto
         self.matriz_colores = matriz_colores
+        self.paleta_rgb = paleta_rgb
 
     def obtener_color(self, x, y):
         """
@@ -22,16 +23,23 @@ class Pattern:
         """
         Crea un patrón a partir de una imagen.
         """
-        from core.image_processor import procesar_imagen
-
-        matriz = procesar_imagen(
-            ruta,
-            ancho,
-            alto,
-            cantidad_colores
+        from core.image_processor import (
+            cargar_imagen,
+            redimensionar_imagen,
+            reducir_colores,
+            crear_matriz_colores,
+            obtener_paleta_rgb
         )
+        
+        imagen = cargar_imagen(ruta)
+        imagen = redimensionar_imagen(imagen, ancho, alto)
+        imagen = reducir_colores(imagen, cantidad_colores)
+        
+        matriz = crear_matriz_colores(imagen)
+        paleta_rgb = obtener_paleta_rgb(imagen)
+        
 
-        return cls(ancho, alto, matriz)
+        return cls(ancho, alto, matriz, paleta_rgb)
 
     def total_casillas(self):
         """
@@ -53,3 +61,53 @@ class Pattern:
                     conteo[indice] = 1
 
         return conteo
+    
+    def cantidad_color(self, indice):
+        """
+        Devuelve cuantas casillas utilizan un color especifico
+        """
+        conteo = self.contar_colores()
+        return conteo.get(indice, 0)
+    
+    def colores_utilizados(self):
+        """
+        Devuelve los indices de los colores utilizados en el patron.
+        """
+        conteo = self.contar_colores()
+        return list(conteo.keys())
+    
+    def obtener_color_rgb(self, indice):
+        """
+        Devuelve el color RGB correspondiente a un indice.
+        """
+        return self.paleta_rgb[indice]
+    
+    def informacion_colores(self):
+        """
+        Devuelve informacion basica de los colores utilizados.
+        """
+        colores = []
+        
+        for indice in self.colores_utilizados():
+            colores.append({
+                "indice": indice,
+                "rgb": self.obtener_color_rgb(indice),
+                "cantidad": self.cantidad_color(indice)
+            })
+        return colores
+    
+    def informacion_casilla(self, x, y):
+        """
+        Devuelve la informacion de una casilla del patron.
+        """
+        
+        indice = self.obtener_color(x, y)
+        rgb = self.obtener_color_rgb(indice)
+        
+        return{
+            "x": x,
+            "y": y,
+            "indice": indice,
+            "rgb": rgb
+        }
+        
